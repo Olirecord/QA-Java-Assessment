f4d324252192b1f457d83b14bf62796ec61a3d42
Oli <Oli.boli@holi.trolly>
2019-10-02 22:49:15 +0100

    Part 1

---
+++ JavaAnswers.java
@@ -0,0 +1,17 @@
+package com.qa.javaAssessment;
+
+public class JavaAnswers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Assessment ref = new Assessment();
+		
+		System.out.println(ref.evenlySpaced(4,6,10));
+		
+		}
+
+	}
+
+
